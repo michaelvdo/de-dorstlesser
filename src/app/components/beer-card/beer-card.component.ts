0950417cadf0a5acff8470d0100c9a4536d12a6c
Michael van den Oudenalder <michael.oudenalder@valtech.com>
2019-12-21 21:49:32 +0100

    Implemented the passing of beer data to beer-card child.

--- src/app/components/beer-card/beer-card.component.ts
+++ src/app/components/beer-card/beer-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-beer-card',
@@ -7,39 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BeerCardComponent implements OnInit {
 
+  @Input() beer;
   unknownMessage = 'Onbekend';
-  beers = [
-    {
-      "id": 1,
-      "Bier": "1410",
-      "Stijl": "speciaalbier",
-      "Stamwortgehalte": "13.0",
-      "Alcoholpercentage": "5,5%",
-      "Gisting": "hoog",
-      "Sinds": 2010,
-      "Brouwerij": "SNAB"
-    },
-    {
-      "id": 2,
-      "Bier": "Alfa Edel Pils",
-      "Stijl": "pilsener",
-      "Stamwortgehalte": "12.0",
-      "Alcoholpercentage": "5,0%",
-      "Gisting": "laag",
-      "Sinds": null,
-      "Brouwerij": "Alfa"
-    },
-    {
-      "id": 3,
-      "Bier": "Alfa Bokbier",
-      "Stijl": "herfstbok",
-      "Stamwortgehalte": "16.3",
-      "Alcoholpercentage": "6,5%",
-      "Gisting": "laag",
-      "Sinds": 1981,
-      "Brouwerij": "Alfa"
-    }
-  ];
 
   constructor() { }
 
