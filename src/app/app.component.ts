3986bf2a6522118b658b251e34a778c5f3e29ce1
Michael van den Oudenalder <michael.oudenalder@valtech.com>
2020-01-09 18:07:13 +0100

    Added mock beers to FIrebase and added connection with Firebase db.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { BEERS } from './data/mock-beers';
 import { Beer } from './models/beer.model';
 import { SanitizeBeersService } from './services/sanitize-beers.service';
+import * as firebase from 'firebase/app';
+import 'firebase/database';
 
 @Component({
   selector: 'app-root',
@@ -13,8 +15,28 @@ export class AppComponent {
   beers: Beer[] = BEERS;
   filteredBeers: Beer[] = BEERS;
 
+  firebaseConfig = {
+    apiKey: "AIzaSyCzwqdG-qCI9JEpjiTyzf7EQqr5Hs7SdcE",
+    authDomain: "de-dorstlesser.firebaseapp.com",
+    databaseURL: "https://de-dorstlesser.firebaseio.com",
+    projectId: "de-dorstlesser",
+    storageBucket: "de-dorstlesser.appspot.com",
+    messagingSenderId: "510738052599",
+    appId: "1:510738052599:web:ac05a2a1d0c3cdcd552af2"
+  }
+
+  database;
+
   constructor(private sanitizeBeers: SanitizeBeersService) {
     this.sanitizeBeers.sanitize(this.beers);
+
+    firebase.initializeApp(this.firebaseConfig);
+
+    this.database = firebase.database();
+
+    this.database.ref('Beers/').once('value', (snap) => {
+      console.log(snap.val());
+    });
   }
 
   /**
